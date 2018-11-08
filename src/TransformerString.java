5e5925c45aba83f524470a68a209e4e4e675212c
maxichz <m3dmx@inbox.lv>
2018-11-08 02:45:30 +0100

    First commit

--- src/TransformerString.java
+++ src/TransformerString.java
@@ -1,15 +1,18 @@
 
 public class TransformerString {
 
-    public String transform(final String sentence) {
-        final String[] words = sentence.split(" ");
-
-        for (int i = 0; i < words.length; i++) {
-            if(words[i].length() == 2) {
-                words[i] = words[i].toUpperCase();
+    public static String transform(String sentence) throws IncorrectDataInputException {
+        if (sentence != null) {
+            final String[] words = sentence.split(" ");
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].length() == 2) {
+                    words[i] = words[i].toUpperCase();
+                }
             }
+            return String.join(" ", words);
+        } else {
+            throw new IncorrectDataInputException("input is null");
         }
-        return String.join(" ", words);
     }
 }
 
